99f53458cf566f16622854b6aba7ce0690653688
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-01-21 17:49:40 +0300

    update the outputs var for the VM

--- terraform/modules/virtual_machine/outputs.tf
+++ terraform/modules/virtual_machine/outputs.tf
@@ -1,9 +1,10 @@
 output "public_ip" {
-  description = "Specifies the public IP address of the virtual machine"
-  value       = azurerm_linux_virtual_machine.virtual_machine.public_ip_address
+  description = "Specifies the public IP address of the virtual machine (if one is created)."
+  # If a public IP exists, output its IP address; otherwise, output an empty string.
+  value       = length(azurerm_public_ip.public_ip) > 0 ? azurerm_public_ip.public_ip[0].ip_address : ""
 }
 
 output "username" {
-  description = "Specifies the username of the virtual machine"
+  description = "Specifies the username of the virtual machine."
   value       = var.vm_user
 }

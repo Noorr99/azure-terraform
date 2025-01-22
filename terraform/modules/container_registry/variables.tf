48775d507b89f766f84a4ce5c3fad1455db89ff4
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-01-22 11:51:36 +0300

    comment out the logs var

--- terraform/modules/container_registry/variables.tf
+++ terraform/modules/container_registry/variables.tf
@@ -41,8 +41,9 @@ variable "georeplication_locations" {
   type        = list(string)
   default     = []
 }
-
+/*
 variable "log_analytics_workspace_id" {
   description = "Specifies the log analytics workspace id"
   type        = string
 }
+/*

dfa6e309fd4ce47de24b18b9527af02e3ecd3328
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-01-23 11:31:58 +0300

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,13 +57,15 @@ module "databricks_subnets" {
   source                           = "./modules/azure-databricks-subnets"
   subnet_name_prefix               = var.databricks_subnet_name_prefix
   vnet_resource_group_name         = azurerm_resource_group.rg.name
-  vnet_name                        = var.aks_vnet_name
+  vnet_name                        = var.aks_vnet_name  # Changed from module.vnet.vnet_name to var.aks_vnet_name
   private_subnet_address_prefixes  = var.databricks_private_subnet_address_prefix
   public_subnet_address_prefixes   = var.databricks_public_subnet_address_prefixes
   additional_service_endpoints     = var.databricks_additional_service_endpoints
   service_delegation_actions       = var.databricks_service_delegation_actions
   tags                             = var.tags
 }
+
+
 
 # Azure Databricks Security Groups Module
 module "databricks_security_groups" {
@@ -93,6 +95,7 @@ module "databricks_workspace" {
 
   tags                = var.tags
 }
+
 
 
 # Virtual Machine Module
--- terraform/modules/azure-databricks-workspace/main.tf
+++ terraform/modules/azure-databricks-workspace/main.tf
@@ -9,13 +9,13 @@ terraform {
 }
 
 resource "azurerm_databricks_workspace" "module-databricks" {
-  name                = var.workspace_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = "standard"
+  name                        = var.workspace_name
+  resource_group_name         = var.resource_group_name
+  location                    = var.location
+  sku                         = "standard"
   managed_resource_group_name = "${var.workspace_name}-managed-rg"
 
-  network {
+  custom_parameters {
     no_public_ip       = false
     private_subnet_id  = var.private_subnet_id
     public_subnet_id   = var.public_subnet_id
@@ -24,3 +24,4 @@ resource "azurerm_databricks_workspace" "module-databricks" {
 
   tags = var.tags
 }
+

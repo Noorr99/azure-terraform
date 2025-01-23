4b54cf41395baf0226f2f2061fd9739c6a44bf8c
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-01-23 20:22:11 +0300

    update to use resource not module

--- terraform/main.tf
+++ terraform/main.tf
@@ -333,12 +333,12 @@ module "datalake_private_dns_zone" {
 
 module "datalake_private_endpoint" {
   source                         = "./modules/private_endpoint"
-  name                           = "${module.datalake_storage_account.name}PrivateEndpoint"
+  name                           = "${azurerm_storage_account.datalake_storage_account.name}PrivateEndpoint"
   location                       = var.location
   resource_group_name            = azurerm_resource_group.rg.name
   subnet_id                      = module.vnet.subnet_ids[var.pe_subnet_name]
   tags                           = var.tags
-  private_connection_resource_id = module.datalake_storage_account.id
+  private_connection_resource_id = azurerm_storage_account.datalake_storage_account.id
   is_manual_connection           = false
   subresource_name               = "dfs"
   private_dns_zone_group_name    = "DatalakePrivateDnsZoneGroup"

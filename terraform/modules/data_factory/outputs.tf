4dd0d6cb4252428d26985c9a28039bb2fc0f0c8d
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-01-26 22:56:49 +0300

    update

--- terraform/modules/data_factory/outputs.tf
+++ terraform/modules/data_factory/outputs.tf
@@ -8,12 +8,3 @@ output "data_factory_name" {
   value       = azurerm_data_factory.this.name
 }
 
-output "managed_ir_id" {
-  description = "The ID of the Managed Integration Runtime."
-  value       = azurerm_data_factory_integration_runtime_managed.azure_ir.id
-}
-
-output "self_hosted_ir_id" {
-  description = "The ID of the Self-Hosted Integration Runtime."
-  value       = azurerm_data_factory_integration_runtime_self_hosted.self_hosted_ir.id
-}

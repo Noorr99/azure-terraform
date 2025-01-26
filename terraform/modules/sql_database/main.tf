a62456b9d61660cec43f56a378af9fc54041ba31
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-01-26 22:23:33 +0300

    update

--- terraform/modules/sql_database/main.tf
+++ terraform/modules/sql_database/main.tf
@@ -1,35 +1,35 @@
-# SQL Server Resource
-resource "azurerm_sql_server" "sql_server" {
+resource "azurerm_mssql_server" "sql_server" {
   name                         = var.sql_server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
+  public_network_access_enabled = false
   tags                         = var.tags
 }
 
-# SQL Database Resource
 resource "azurerm_sql_database" "sql_database" {
   name                = var.sql_database_name
   resource_group_name = var.resource_group_name
+  server_name         = azurerm_mssql_server.sql_server.name
   location            = var.location
-  server_name         = azurerm_sql_server.sql_server.name
   edition             = var.sql_database_tier
-  requested_service_objective_name = "P1" # Premium tier, P1: 125 DTUs
+  requested_service_objective_name = "P1" # Premium, P1: 125 DTUs
   max_size_gb         = var.sql_database_size_gb
+  zone_redundant      = true
   tags                = var.tags
 }
 
-# Backup Retention Policy (if required)
-resource "azurerm_backup_policy_vm" "long_term_retention" {
-  name                = "${var.sql_server_name}-backup-policy"
-  resource_group_name = var.resource_group_name
+resource "azurerm_private_endpoint" "sql_private_endpoint" {
+  name                = "${var.sql_server_name}-pe"
   location            = var.location
-  retention_policy {
-    daily_schedule {
-      retention_duration = "${var.long_term_retention_backup}GB"
-    }
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "sqlServerConnection"
+    private_connection_resource_id = azurerm_mssql_server.sql_server.id
+    subresource_names              = ["sqlServer"]
   }
-  tags = var.tags
 }
